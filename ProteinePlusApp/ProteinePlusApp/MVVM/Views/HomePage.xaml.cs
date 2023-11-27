2f686404cd2a393787ee9a8d052b392385bdddb4
ChristopherDronjak <yoschmoo@gmail.com>
2023-11-27 16:29:23 +1300

    added feedback button

--- ProteinePlusApp/ProteinePlusApp/MVVM/Views/HomePage.xaml.cs
+++ ProteinePlusApp/ProteinePlusApp/MVVM/Views/HomePage.xaml.cs
@@ -1,4 +1,6 @@
 using SQLite;
+using System.Diagnostics;
+
 namespace ProteinePlusApp.MVVM.Views;
 
 public partial class HomePage : ContentPage
@@ -20,5 +22,10 @@ public partial class HomePage : ContentPage
     private void Workoutcreate_Clicked(object sender, EventArgs e)
     {
         Navigation.PushAsync(new NavigationPage(new WorkoutCreatePage(dbService)));
+    }
+
+    private async void feedback_Clicked(object sender, EventArgs e)
+    {
+        await Launcher.Default.OpenAsync("https://docs.google.com/forms/d/e/1FAIpQLSconRpCWMBcZoGsMC5U828ueOfhJchy4YADrgj-Eucoeob2hA/viewform?usp=sf_link");
     }
 }
