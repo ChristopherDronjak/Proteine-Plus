d0e95d05ffbb624e409aee69bd3d0bb80d00e28f
ChristopherDronjak <yoschmoo@gmail.com>
2023-12-04 22:50:29 +1300

    added some comments

--- ProteinePlusApp/ProteinePlusApp/MVVM/Views/TemplateRecipes.xaml.cs
+++ ProteinePlusApp/ProteinePlusApp/MVVM/Views/TemplateRecipes.xaml.cs
@@ -7,16 +7,19 @@ public partial class TemplateRecipes : ContentPage
 		InitializeComponent();
 	}
 
+    //button to go to recipe page
     private void spanishpisto_Clicked(object sender, EventArgs e)
     {
         Navigation.PushAsync(new NavigationPage(new SpanishPisto()));
     }
 
+    //button to go to recipe page
     private void lasagne_Clicked(object sender, EventArgs e)
     {
         Navigation.PushAsync(new NavigationPage(new Lasagne()));
     }
 
+    //button to go to recipe page
     private void wholewheatgarlicbread_Clicked(object sender, EventArgs e)
     {
         Navigation.PushAsync(new NavigationPage(new WholeWheatGarlicBread()));
